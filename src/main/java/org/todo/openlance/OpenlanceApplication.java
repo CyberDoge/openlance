4b03b65f419dc885c86c1dd7d34dc4983bbcdbbf
Pauls <ganin.paul@gmail.com>
2018-09-29 13:30:07 +0400

    init

--- src/main/java/org/todo/openlance/OpenlanceApplication.java
+++ src/main/java/org/todo/openlance/OpenlanceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OpenlanceApplication {
-
+ //start point
 	public static void main(String[] args) {
 		SpringApplication.run(OpenlanceApplication.class, args);
 	}
